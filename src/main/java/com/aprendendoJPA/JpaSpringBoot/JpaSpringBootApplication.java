f18dd2d94fab410aa82a945b776f6b55b7001cf2
HigorLucas <higor.freitas@neppoadm.com.br>
2019-11-03 19:11:41 -0300

    Add inicialize categoria

--- src/main/java/com/aprendendoJPA/JpaSpringBoot/JpaSpringBootApplication.java
+++ src/main/java/com/aprendendoJPA/JpaSpringBoot/JpaSpringBootApplication.java
@@ -1,13 +1,32 @@
 package com.aprendendoJPA.JpaSpringBoot;
 
+import com.aprendendoJPA.JpaSpringBoot.domain.Categoria;
+import com.aprendendoJPA.JpaSpringBoot.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class JpaSpringBootApplication {
+public class JpaSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpaSpringBootApplication.class, args);
+
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria("Informatica");
+		Categoria cat2 = new Categoria("Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
