06ce284c1aefcb3c2534dcc850c3ae8d08ebca61
HigorLucas <higor.freitas@neppoadm.com.br>
2019-11-03 12:18:28 -0300

    init project

---
+++ src/test/java/com/aprendendoJPA/JpaSpringBoot/JpaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendendoJPA.JpaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
