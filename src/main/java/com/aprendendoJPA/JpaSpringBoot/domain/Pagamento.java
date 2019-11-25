14fd0de2e239734d922f26a1dbe83fec2f42727f
HigorLucas <higor.freitas@neppoadm.com.br>
2019-11-25 19:39:11 -0300

    Adicionando metodo de pagamento

--- src/main/java/com/aprendendoJPA/JpaSpringBoot/domain/Pagamento.java
+++ src/main/java/com/aprendendoJPA/JpaSpringBoot/domain/Pagamento.java
@@ -1,4 +1,105 @@
 package com.aprendendoJPA.JpaSpringBoot.domain;
 
-public class Pagamento {
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import com.aprendendoJPA.JpaSpringBoot.domain.enums.EstadoPagamento;
+
+
+@Entity
+@Inheritance( strategy = InheritanceType.JOINED )
+public class Pagamento implements Serializable {
+
+    private static final long serialVersionID = 1L;
+
+    @Id
+    private Integer id;
+
+    private Integer estado;
+
+    @OneToOne
+    @JoinColumn( name = "pedido_id" )
+    @MapsId
+    private Pedido pedido;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido ) {
+
+        this.id = id;
+        this.estado = estado.getCod();
+        this.pedido = pedido;
+    }
+
+
+    public Integer getId() {
+
+        return id;
+    }
+
+
+    public void setId( Integer id ) {
+
+        this.id = id;
+    }
+
+
+    public EstadoPagamento getEstado() {
+
+        return EstadoPagamento.toEnum( estado );
+    }
+
+
+    public void setEstado( EstadoPagamento estado ) {
+
+        this.estado = estado.getCod();
+    }
+
+
+    public Pedido getPedido() {
+
+        return pedido;
+    }
+
+
+    public void setPedido( Pedido pedido ) {
+
+        this.pedido = pedido;
+    }
+
+
+    @Override
+    public boolean equals( Object o ) {
+
+        if ( this == o )
+            return true;
+        if ( o == null || getClass() != o.getClass() )
+            return false;
+        Pagamento pagamento = (Pagamento) o;
+        return Objects.equals( id, pagamento.id );
+    }
+
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash( id );
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "Pagamento{" + "id=" + id + ", estado=" + estado + ", pedido=" + pedido + '}';
+    }
 }
--- src/main/java/com/aprendendoJPA/JpaSpringBoot/domain/Pedido.java
+++ src/main/java/com/aprendendoJPA/JpaSpringBoot/domain/Pedido.java
@@ -1,4 +1,138 @@
 package com.aprendendoJPA.JpaSpringBoot.domain;
 
-public class Pedido {
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Pedido implements Serializable {
+
+    private static final long serialVersionID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Date instante;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
+    private Pagamento pagamento;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "endereco_de_entrega_id")
+    private Endereco enderecoEntrega;
+
+
+    public Pedido() {
+
+    }
+
+
+    public Pedido( Integer id, Date instante, Cliente cliente, Endereco enderecoEntrega ) {
+
+        this.id = id;
+        this.instante = instante;
+        this.cliente = cliente;
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+
+    public Integer getId() {
+
+        return id;
+    }
+
+
+    public void setId( Integer id ) {
+
+        this.id = id;
+    }
+
+
+    public Date getInstante() {
+
+        return instante;
+    }
+
+
+    public void setInstante( Date instante ) {
+
+        this.instante = instante;
+    }
+
+
+    public Pagamento getPagamento() {
+
+        return pagamento;
+    }
+
+
+    public void setPagamento( Pagamento pagamento ) {
+
+        this.pagamento = pagamento;
+    }
+
+
+    public Cliente getCliente() {
+
+        return cliente;
+    }
+
+
+    public void setCliente( Cliente cliente ) {
+
+        this.cliente = cliente;
+    }
+
+
+    public Endereco getEnderecoEntrega() {
+
+        return enderecoEntrega;
+    }
+
+
+    public void setEnderecoEntrega( Endereco enderecoEntrega ) {
+
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+
+    @Override
+    public boolean equals( Object o ) {
+
+        if ( this == o )
+            return true;
+        if ( o == null || getClass() != o.getClass() )
+            return false;
+        Pedido pedido = (Pedido) o;
+        return Objects.equals( id, pedido.id );
+    }
+
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash( id );
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "Pedido{" + "id=" + id + ", instante=" + instante + ", pagamento=" + pagamento + ", cliente=" + cliente + ", enderecoEntrega=" + enderecoEntrega
+            + '}';
+    }
 }
